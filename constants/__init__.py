c302ec25f051aee3917ec31e5faef9b3d0e64a9c
Libavi <libavi@e.mail.de>
2026-03-02 23:29:14 +0100

    Added constants, adapted from https://github.com/Krakenmeister/genieutils-examples.

---
+++ constants/__init__.py
@@ -0,0 +1,3 @@
+from . import techs, unit_classes, armor_classes, units, resources, civilizations, command_types, attributes
+
+__all__ = ["techs", "unit_classes", "armor_classes", "units", "resources", "civilizations", "command_types", "attributes"]
